90abd0fcd1a34b6384ddbed6066a94ab24252f67
Maximo Hinojosa <maximohinojosa13@gmail.com>
2018-10-30 00:34:00 -0700

    Users can see single reviews

--- app.js
+++ app.js
@@ -51,15 +51,24 @@ app.get('/reviews/new', (req, res) => {
 })
 
 
-//CREATE
+// CREATE
 app.post('/reviews', (req, res) => {
-    Review.create(req.body).then((review) => {
-        console.log(review);
-        res.redirect('/');
-    })
-    .catch((err) => { console.log(err.message);
-    })
+  Review.create(req.body).then((review) => {
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
 })
+//SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
+  }).catch((err) => {
+    console.log(err.message);
+  })
+})
+
 
 //APPLICATION WAITING FOR ROUTES TO BE INSTANTIATED
 // app.listen(port, () => {
