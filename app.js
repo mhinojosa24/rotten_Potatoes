b21e2441b2ab52320e63c8fdaf2852b4edd7978f
Maximo Hinojosa <maximohinojosa13@gmail.com>
2018-11-01 02:05:55 -0700

    user can delete reviews

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // initilize template
 const express = require('express');
+const methodOverride = require('method-override');
 const exphbs = require('express-handlebars');
-const app = express();
 const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 
@@ -15,6 +15,7 @@ const Review = mongoose.model('Review', {
     movieTitle: String,
     description: String
 });
+const app = express();
 
 // MiddleWare
 app.engine('.hbs', exphbs({
@@ -22,18 +23,13 @@ app.engine('.hbs', exphbs({
     defaultLayout: 'main'
 }));
 
+
 app.set('view engine', '.hbs');
 app.use(bodyParser.urlencoded({ extended: true }));
+// Override with POST having ?_method=DELETE or ?_method=PUT
+app.use(methodOverride('_method'))
 
-//mock array
-//JS object
-// let reviews = [
-//     { title: "Great", movieTitle: "Batman II" },
-//     { title: "Awesome Movie", movieTitle: "Titanic" },
-//     { title: ""}
-//
-
-//Index
+// Index
 app.get('/', (req, res) => {
   Review.find()
     .then(reviews => {
@@ -45,7 +41,7 @@ app.get('/', (req, res) => {
 })
 
 
-//NEW
+// NEW
 app.get('/reviews/new', (req, res) => {
     res.render('reviews-new', {});
 })
@@ -55,24 +51,44 @@ app.get('/reviews/new', (req, res) => {
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
     console.log(review)
-    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
-  }).catch((err) => {
+    res.redirect(`/reviews/${review._id}`) }).catch((err) => {
     console.log(err.message)
-  })
-})
-//SHOW
+    })
+});
+// SHOW
 app.get('/reviews/:id', (req, res) => {
-  Review.findById(req.params.id).then((review) => {
+    Review.findById(req.params.id).then((review) => {
     res.render('reviews-show', { review: review })
-  }).catch((err) => {
-    console.log(err.message);
-  })
-})
+    }).catch((err) => {
+        console.log(err.message)
+    });
+});
 
+// EDIT
+app.get('/reviews/:id/edit', (req, res) => {
+    Review.findById(req.params.id, function(err, review) {
+        res.render('reviews-edit', { review: review });
+    });
+});
 
-//APPLICATION WAITING FOR ROUTES TO BE INSTANTIATED
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}!`);
-// })
+// UPDATE
+app.put('/reviews/:id', (req, res) => {
+    Review.findByIdAndUpdate(req.params.id, req.body)
+        .then(review => {
+            res.redirect(`/reviews/${review._id}`)
+        }).catch(err => {
+            console.log(err.message)
+        });
+});
+
+// DELETE
+app.delete('/reviews/:id', (req, res) => {
+    console.log("DELETE review")
+    Review.findByIdAndRemove(req.params.id).then((review) => {
+        res.redirect('/');
+    }).catch((err) => {
+        console.log(err.message);
+    });
+});
 
 app.listen(port);
