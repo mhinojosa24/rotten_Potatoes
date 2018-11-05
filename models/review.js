1476fd92fdd54dd74d37bfa50b02268ee47d7dbb
Maximo Hinojosa <maximohinojosa13@gmail.com>
2018-11-05 06:59:49 -0800

    Added vanilla boostrap

---
+++ models/review.js
@@ -0,0 +1,10 @@
+// models/review.js
+const mongoose = require('mongoose');
+
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String,
+    description: String
+});
+
+module.exports = Review;
---
+++ server.js
@@ -0,0 +1,29 @@
+// initilize template
+const express = require('express');
+const methodOverride = require('method-override');
+const exphbs = require('express-handlebars');
+const app = express();
+const port = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const Review = require('./models/review');
+// const Common = require('./models/comment');
+
+
+
+// MiddleWare
+app.engine('.hbs', exphbs({ extname: '.hbs', defaultLayout: 'main'}));
+app.set('view engine', '.hbs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+
+const reviews = require('./controllers/reviews')(app);
+// reviews(app);
+
+
+if(!module.parent) {
+    app.listen(port);
+}
+
+module.exports = app;
